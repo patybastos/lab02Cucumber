5cd17b78b891ed01e1e3e94d6811bcceedbe503f
rodolfodrago <rodolfo.drago@gmail.com>
2016-10-08 10:55:46 -0300

    Commit com todas as classes necessárias (a casca está pronta)

---
+++ bookstore-correios/src/test/java/br/unicamp/bookstore/steps/BuscarStatusEntregaSteps.java
@@ -0,0 +1,21 @@
+package br.unicamp.bookstore.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+/*
+public class BuscarStatusEntregaSteps {
+  @Given(^you are in Given annotation$)
+  public void given() throws Throwable {
+  }
+
+  @When(^you are in When annotation$)
+  public void when() throws Throwable {
+  }
+
+  @Then(^you are in Then annotation$)
+  public void then() throws Throwable {
+  }
+
+}*/
